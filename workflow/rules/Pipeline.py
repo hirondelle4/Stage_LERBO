e8ae0b62d3e82fb09d9c5c9faa3431adc399d87c
Mercier <merciereva93@gmail.com>
2020-07-02 14:22:52 +0200

    config

--- workflow/rules/Pipeline.py
+++ workflow/rules/Pipeline.py
@@ -4,6 +4,8 @@
 ## mamba create -c conda-forge -c bioconda -n snakemake snakemake
 
 ########################################################
+configfile: "config.yaml"
+
 rule all:
     input:
         "qualityC/pycoQC_output_min_pass_q10.html"
@@ -20,3 +22,11 @@ rule qualityC:
         "/home/hirondelle/miniconda3/envs/pycoqc.yaml"
     shell:
         "pycoQC  --summary_file {input} --html_outfile {output} --min_pass_qual 10 2> {log}"
+
+rule rename:
+    input:
+        a="{datasets}"
+        b="{samples_names}"
+    output:
+        "resources/{samples}"
+    shell:
